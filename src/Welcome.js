1cbc89f5b230b2a60508a156ec5d45abd7bd3153
unknown <Gejjeg>
2023-02-01 13:13:33 +0100

    feat: props - 02 added

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 export class Welcome extends React.Component {
+    static defaultProps = {
+        name: "Peter"
+    };
     render() {
         return <p>Welcome, {this.props.name}</p>
-    }
-}
+    };
+};
